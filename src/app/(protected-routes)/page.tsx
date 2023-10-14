24bc747becf579b242a372fa04086b45ea35ae0d
Sergio <sergio.rodriguezaramayo@gmail.com>
2023-10-14 19:55:16 -0400

    Final

--- src/app/(protected-routes)/page.tsx
+++ src/app/(protected-routes)/page.tsx
@@ -1,8 +1,13 @@
+import { ProductsListClient } from "@/products";
+import TasksPage from "./tasks/page";
 
 export default function HomePage() {
   return (
+    
     <div>
-      <h1>Hello Page 1</h1>
+      {/* <h1 className="">Hello Page 1</h1> */}
+      <TasksPage/>
+      <ProductsListClient />
     </div>
   );
 }
